cff099a1b1afa1e47c509a1276c909b5f5cfffa5
Alexander Todorov <41835213+tdrv90@users.noreply.github.com>
2020-04-26 10:10:24 +0300

    adding exercises 1-20

---
+++ 12_prompting_people/ex12.py
@@ -0,0 +1,4 @@
+age = int(input("How old are you? "))
+height = int(input("How tall are you? "))
+weight = int(input("How much do you weigh? "))
+print("So, you're %r old, %r tall and %r heavy." % (age, height, weight))
